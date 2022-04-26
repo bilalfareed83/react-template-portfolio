30e52e93ae4a610503f5a7fcc5af78dd5884915b
bilalfareed83 <74311496+cssrgit@users.noreply.github.com>
2022-04-26 11:12:12 +0500

    optimize img

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
 import "./App.css";
-import { BioData } from "./components/bio/bioData";
-import { Contact } from "./components/contact/contact";
-import { Counter } from "./components/counter/counter";
-import { Footer } from "./components/footer/footer";
-import { HeroComponent } from "./components/hero/hero";
-import { HireMe } from "./components/hireMe/hireMe";
-import { Portfolio } from "./components/portfolio/portfolio";
 import { RoutesComponent } from "./components/routes/routes";
-import { ScrollTop } from "./components/scrollTop/scrollTop";
-import { Service } from "./components/service/service";
-import Testimonial from "./components/testimonial/testimonial";
 
 function App() {
   return (
     <div className="App">
       <RoutesComponent />
-      {/* <HeroComponent />
-      <BioData />
-      <Portfolio />
-      <Counter />
-      <Service />
-      <Testimonial />
-      <HireMe />
-      <Contact />
-      <Footer />
-      <ScrollTop /> */}
     </div>
   );
 }
